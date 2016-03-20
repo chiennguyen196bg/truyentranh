a4c79922dea683c027a20d5bbc8af1b4f157d2ca
chien nguyen <chienminhnguyen196@gmail.com>
2016-03-20 15:46:13 +0700

    fix summary

--- public/js/Controller/randomController.js
+++ public/js/Controller/randomController.js
@@ -2,6 +2,13 @@ app.controller('randomController', function($scope, $http) {
 	$http.get("/json/random")
     .then(function(response) {
         $scope.itemRandom = response.data;
+        if ($scope.itemRandom.summary == ""){
+        	$scope.itemRandom.summary = "Truyện tranh 
+        	<strong>"+$scope.itemRandom.name+"</strong>
+        	 nội dung đang được cập nhật. 
+        	 Ghé thăm Truyen47.com để được đọc các chương mới nhất của 
+        	 <strong>"+$scope.itemRandom.name+"</strong>"
+        }
         // $scope.statuscode = response.status;
        	// $scope.statustext = response.statustext;
     });
